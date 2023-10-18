94ae1a944855d20f23afcc445fe057005f976744
Bandi Murali Chowdary <97547174+bandimuralichowdary@users.noreply.github.com>
2023-10-18 09:17:24 +0530

    Add files via upload

---
+++ PROJECT-1/process_category.php
@@ -0,0 +1,17 @@
+<!-- PROJECT-1 MAIN PHP-->
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["category"])) {
+        $category = $_POST["category"];
+
+        // Redirect to the appropriate page based on the selected category
+        if ($category === "farmer") {
+            header("Location: farmer.html");
+            exit();
+        } elseif ($category === "consumer") {
+            header("Location: consumer.html");
+            exit();
+        }
+    }
+}
+?>
